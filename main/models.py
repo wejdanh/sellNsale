1ad8b0e72d035672903250a840ab993fae1dfe56
YasserYka <yasseryka@gmail.com>
2019-08-24 18:30:27 +0300

    set up

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    useName = models.CharField(max_length=64)
+    phoneNumber = models.IntegerField(max_length=10)
